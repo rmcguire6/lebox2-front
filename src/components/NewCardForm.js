e3763f186fa7f4d7e8aa891fb112f464816d878e
Regina McGuire <rmcguire@as.net>
2022-01-28 17:25:10 -0800

    feat: Create state in NewCardForm

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,13 +1,21 @@
+import {useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 export const NewCardForm = ({createCard}) => {
+  const [question, setQuestion] = useState('');
   const handleSubmit = e => {
     e.preventDefault();
-    createCard();
+    createCard(question);
   };
   return (
     <form onSubmit={handleSubmit}>
-      <TextField placeholder="Add a Card" fullWidth variant="filled" />
+      <TextField
+        value={question}
+        onChange={e => setQuestion(e.target.value)}
+        placeholder="Add a Card"
+        fullWidth
+        variant="filled"
+      />
       <Button
         type="submit"
         variant="contained"
