3a617f062a4228ab6a977723217bbe12fb045787
Regina McGuire <rmcguire@as.net>
2022-03-04 17:25:36 -0800

    tests: Refactor listing-cards-spec to use fixture

--- cypress/integration/cards/listing-cards.spec.js
+++ cypress/integration/cards/listing-cards.spec.js
@@ -1,48 +1,10 @@
 describe('Listing Cards', () => {
   it('shows cards for one user', () => {
-    const cardsData = [
-      {
-        subject: 'Spanish',
-        question: 'vivir',
-        answer: 'to live',
-        creatorId: 1,
-        isActive: true,
-      },
-      {
-        subject: 'Spanish',
-        question: 'tomar',
-        answer: 'to take',
-        creatorId: 1,
-        isActive: true,
-      },
-      {
-        subject: 'Spanish',
-        question: 'comer',
-        answer: 'to eat',
-        creatorId: 1,
-        isActive: true,
-      },
-      {
-        subject: 'Spanish',
-        question: 'escribir',
-        answer: 'to write',
-        creatorId: 1,
-        isActive: true,
-      },
-    ];
+    cy.server();
 
-    cy.server({force404: true});
-
-    cy.route({
-      method: 'GET',
-      url: 'http://localhost:8000/test_cards/',
-      response: cardsData,
-    });
+    cy.route('GET', '/test_cards/', 'fixture:cardsData');
 
     cy.visit('/test_cards/');
-    cy.contains(cardsData[0].question);
-    cy.contains(cardsData[1].question);
-    cy.contains(cardsData[2].question);
-    cy.contains(cardsData[3].question);
+    cy.get('p').should('have.length', 3);
   });
 });
