c03bdb7455cbe2c76166cf73d2c61d2c3022ee57
R McGuire <rmcguire@as.net>
2022-06-13 15:02:26 -0700

    feat: Add a 404 route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Signin from './pages/Signin';
 import NavBar from './components/NavBar';
+import NoMatch from './pages/NoMatch'
 import Register from './pages/Register';
 
 import './App.css';
@@ -23,6 +24,7 @@ const App = () => {
             <Route path="/signin" element={<Signin />} />
             <Route path="/register" setUser={setUser} element={<Register />} />
             <Route index element={<Dashboard />} />
+            <Route path="*" element={<NoMatch />} />
           </Routes>
         </BrowserRouter>
       </div>
