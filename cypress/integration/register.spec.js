2e9d99c500a15f506efe2e102c27a17135da2e96
R McGuire <rmcguire@as.net>
2022-08-05 11:11:35 -0700

    task: Lint cypress files

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -1,45 +1,45 @@
 describe('Register form', () => {
-    beforeEach(() => {
-        cy.visit('/register')
-    })
-    it('focuses input on load', () => {
-      cy.focused().should('have.class', 'username');
-    });
-    it('accepts input', () => {
-      cy.get('.username').type('Bob').should('have.value', 
-      'Bob');
+  beforeEach(() => {
+    cy.visit('/register');
+  });
+  it('focuses input on load', () => {
+    cy.focused().should('have.class', 'username');
+  });
+  it('accepts input', () => {
+    cy.get('.username').type('Bob').should('have.value', 'Bob');
+    cy.get('.email').type('bob@as.net').should('have.value', 'bob@as.net');
+    cy.get('.password').type('1234').should('have.value', '1234');
+  });
+  context('new user form submission', () => {
+    it('Adds a new user on submit', () => {
+      cy.intercept('POST', '/users/', {
+        user_id: 999,
+        username: 'Bob',
+        email: 'bob@as.net',
+        current_day_number: 1,
+        created_at: '2022-05-14',
+      }).as('addUser');
+      cy.get('.username').type('Bob').should('have.value', 'Bob');
       cy.get('.email').type('bob@as.net').should('have.value', 'bob@as.net');
       cy.get('.password').type('1234').should('have.value', '1234');
-    });
-    context('new user form submission', () => {
-      it('Adds a new user on submit', () => {
-        cy.intercept('POST', '/users/', {
-          user_id: 999,
-          username: 'Bob',
-          email: 'bob@as.net',
-          current_day_number: 1,
-          created_at: "2022-05-14"
-        }).as('addUser')
-        cy.get('.username').type('Bob').should('have.value', 
-        'Bob');
-        cy.get('.email').type('bob@as.net').should('have.value', 'bob@as.net');
-        cy.get('.password').type('1234').should('have.value', '1234');
-        cy.contains('Create a New Account').click()
-        
-        cy.wait('@addUser').its('response.body').should('exist')
-          .and('contain', {"username":'Bob', "email": 'bob@as.net', "current_day_number":1})
+      cy.contains('Create a New Account').click();
 
+      cy.wait('@addUser').its('response.body').should('exist').and('contain', {
+        username: 'Bob',
+        email: 'bob@as.net',
+        current_day_number: 1,
       });
-      it('Shows an error message on a failed submission', () => {
-        cy.server()
-        cy.route({
-          url: '/users/',
-          method: 'POST',
-          status: 500,
-          response: {},
-        });
-        cy.get('.newUser_button').click();
-        cy.get('.submission-error').should('be.visible');
+    });
+    it('Shows an error message on a failed submission', () => {
+      cy.server();
+      cy.route({
+        url: '/users/',
+        method: 'POST',
+        status: 500,
+        response: {},
       });
-   });
+      cy.get('.newUser_button').click();
+      cy.get('.submission-error').should('be.visible');
+    });
+  });
 });
--- cypress/integration/signin.spec.js
+++ cypress/integration/signin.spec.js
@@ -1,19 +1,20 @@
 describe('Signing in', () => {
-    
-    it('accepts input and returns a token', () => {
-      const formData = new FormData()
-      formData.append('username', 'bob@as.net')
-      formData.append('password', '1234')
-      cy.intercept('POST', '/signin/', {body: formData,
-          headers: {
-            'Content-type': 'multipart/form-data',
-          }}).as('signIn')
-      cy.visit('/signin')
-      cy.get('.email').type('bob@as.net').should('have.value', 'bob@as.net');
-      cy.get('.password').type('1234').should('have.value', '1234')
-      cy.contains('Sign In').click()
-      cy.wait('@signIn').its('request.body').should('contain', {
-        formData: formData
-      })
+  it('accepts input and returns a token', () => {
+    const formData = new FormData();
+    formData.append('username', 'bob@as.net');
+    formData.append('password', '1234');
+    cy.intercept('POST', '/signin/', {
+      body: formData,
+      headers: {
+        'Content-type': 'multipart/form-data',
+      },
+    }).as('signIn');
+    cy.visit('/signin');
+    cy.get('.email').type('bob@as.net').should('have.value', 'bob@as.net');
+    cy.get('.password').type('1234').should('have.value', '1234');
+    cy.contains('Sign In').click();
+    cy.wait('@signIn').its('request.body').should('contain', {
+      formData: formData,
     });
-})
+  });
+});
